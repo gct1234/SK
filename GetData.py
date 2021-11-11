26883b20257a61ed4cae58572cbbf7c7dcac22b9
gct1234 <gct123@sina.com>
2021-11-11 08:49:31 +0800

    修改卖出条件：选出持股期间最高收盘价，超过10%即卖出 修改选股条件：高于100元不选

--- GetData.py
+++ GetData.py
@@ -34,20 +34,21 @@ for i in range(int(df_code.size/5)-1):
     #print(df_code)
     #print(int(df_code.size/5))
     #df_daily = pro.daily(ts_code=df_code.iloc[i,0],strat_date='20180101',end_date='20201231')
-    df_data = ts.pro_bar(ts_code=df_code.iloc[i,0],adj='qfq',start_date='20180101',end_date='20181231',ma=[20])
+    df_data = ts.pro_bar(ts_code=df_code.iloc[i,0],adj='qfq',start_date='20180101',end_date='20181231',ma=[5,10,20])
 
     print(df_code.iloc[i,0],df_code.iloc[i,4])
     if (df_code.iloc[i,4]<'20180101') and (int(df_data.size/13)-1>0) :
         new_date = df_code.iloc[i, 4]
-        for j in range(int(df_data.size/13)-1,0,-1):
+        for j in range(int(df_data.size/17)-1,0,-1):
             if df_data.iloc[j,1] > new_date and df_data.iloc[j,5]<100:
                 max_close = df_data.iloc[j,5]
-#              if (df_data.iloc[j,11]!=0) and (df_data.iloc[j,5]>=df_data.iloc[j,11]*1.1) and (df_data.iloc[j,5]!=0) and (df_data.iloc[j,5]<=df_data.iloc[j,11]*1.4) and (df_data.iloc[j,5]>df_data.iloc[j,2]):
-                if (df_data.iloc[j,11]!=0) and (df_data.iloc[j,5]>=df_data.iloc[j,11]*1.2) and (df_data.iloc[j,5]!=0) and (df_data.iloc[j,5]>df_data.iloc[j,2]) and ((df_data.iloc[j,3]-df_data.iloc[j,5])<(df_data.iloc[j,5]-df_data.iloc[j,2])*0.5):
+#              if (df_data.iloc[j,15]!=0) and (df_data.iloc[j,5]>=df_data.iloc[j,15]*1.1) and (df_data.iloc[j,5]!=0) and (df_data.iloc[j,5]<=df_data.iloc[j,13]*1.4) and (df_data.iloc[j,5]>df_data.iloc[j,2]):
+                if (df_data.iloc[j,15]!=0) and (df_data.iloc[j,5]>=df_data.iloc[j,15]*1.2) and (df_data.iloc[j,5]!=0) and (df_data.iloc[j,5]>df_data.iloc[j,2]) \
+                        and ((df_data.iloc[j,3]-df_data.iloc[j,5])<(df_data.iloc[j,5]-df_data.iloc[j,2])*0.5) and (df_data.iloc[j,11]>df_data.iloc[j,13] and df_data.iloc[j,13]>df_data.iloc[j,15]):
                     y = 0
                     for y in range(int(df_data.size/13)-j):
-#                        if (df_data.iloc[j-y,5]<=df_data.iloc[j,5]*0.97) or (df_data.iloc[j-y,5]<=df_data.iloc[j-y,11]) or (df_data.iloc[j-y,5]>=max_close*1.1) :
-                        if (df_data.iloc[j-y,5]<=df_data.iloc[j,5]*0.95) or (df_data.iloc[j-y,5]<=df_data.iloc[j-y,11])  or (df_data.iloc[j-y,5]>=max_close*1.1):
+#                        if (df_data.iloc[j-y,5]<=df_data.iloc[j,5]*0.97) or (df_data.iloc[j-y,5]<=df_data.iloc[j-y,13]) or (df_data.iloc[j-y,5]>=max_close*1.1) :
+                        if (df_data.iloc[j-y,5]<=df_data.iloc[j,5]*0.95) or (df_data.iloc[j-y,5]<=df_data.iloc[j-y,13])  or (df_data.iloc[j-y,5]>=max_close*1.1):
                             if df_data.iloc[j - y, 5] - df_data.iloc[j, 5] <=0 :
                                 y_k += 1
                             else:
@@ -62,9 +63,14 @@ for i in range(int(df_code.size/5)-1):
                             sheet.cell(row=x,column=7,value = df_data.iloc[j-y,1])    #卖出日期
                             sheet.cell(row=x,column=8,value = df_data.iloc[j-y,5])    #收盘价
                             sheet.cell(row=x,column=9,value = df_data.iloc[j-y,10])    #成交量
-                            sheet.cell(row=x,column=9,value = df_data.iloc[j-y,11])    #20均价
-                            sheet.cell(row=x,column=10,value = df_data.iloc[j-y,5]-df_data.iloc[j,5])    #差价
-                            sheet.cell(row=x,column=11,value = (df_data.iloc[j-y,5]-df_data.iloc[j,5])/df_data.iloc[j,5])    #盈亏比
+                            sheet.cell(row=x,column=10,value = df_data.iloc[j-y,15])    #20均价
+                            if (df_data.iloc[j-y,5]-df_data.iloc[j,5])>0:
+                                sheet.cell(row=x,column=11,value = df_data.iloc[j-y,5]-df_data.iloc[j,5])    #差价
+                                sheet.cell(row=x,column=12,value = (df_data.iloc[j-y,5]-df_data.iloc[j,5])/df_data.iloc[j,5])    #盈亏比
+                            else:
+                                sheet.cell(row=x,column=13,value = df_data.iloc[j-y,5]-df_data.iloc[j,5])    #差价
+                                sheet.cell(row=x,column=14,value = (df_data.iloc[j-y,5]-df_data.iloc[j,5])/df_data.iloc[j,5])    #盈亏比
+
                             x +=  1
                             j -= y-1
                             new_date = df_data.iloc[j-y,1]
